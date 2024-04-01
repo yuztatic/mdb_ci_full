0554216dda607d5c5ad9b5ec66197368f2dca2de
unknown <juliusmanipol@gmail.com>
2024-04-01 18:34:58 +0800

    starts

---
+++ app/Models/DbTransTblHgeModel.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class DbTransTblHgeModel extends Model
+{
+    protected $table            = 'db_trans_tbl_hge';
+    protected $primaryKey       = 'TransRecordID';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = [
+        'RegCode',
+        'EligType',
+        'EffDate',
+        'LName',
+        'FName',
+        'MI',
+        'MName',
+        'ExtName',
+        'MdnName',
+        'Address',
+        'BDate',
+        'BPlace',
+        'Sex',
+        'CivilStat',
+        'Createdby',
+        'CreatedDate',
+        'LastUpdatedby',
+        'LastUpdatedDate',
+        'HEICode',
+        'CourseCode',
+        'Major',
+        'HonorsReceived',
+        'DateGrad'
+    ];
+
+    protected bool $allowEmptyInserts = false;
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
