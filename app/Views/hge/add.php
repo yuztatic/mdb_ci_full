ea7f0b9ac1c77b9954fa7b6b4cdf404639de6a87
unknown <juliusmanipol@gmail.com>
2024-04-02 16:16:35 +0800

    form updated up to maiden name

--- app/Views/hge/add.php
+++ app/Views/hge/add.php
@@ -8,8 +8,8 @@
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Transactions</a></li>
-                        <li class="breadcrumb-item"><a href="#">Spels Database</a></li>
+                        <li class="breadcrumb-item"><a href="#">Transaction Dbase</a></li>
+                        <li class="breadcrumb-item"><a href="#">Spels Dbase</a></li>
                     </ol>
                 </div>
             </div>
@@ -17,22 +17,75 @@
 
             <div class="card card-info">
                 <div class="card-header bg-teal">
-                    <h3 class="card-title">Personal Information</h3>
+                    <h3 class="card-title">A. Eligibility Information Section</h3>
                 </div>
-                <div class="card-body">
+                <form>
+                    <div class="card-body">
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label for="RegCode">Region Code</label>
+                                <input type="text" class="form-control" id="RegCode" name="RegCode" length="2">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="EligType">Eligibility Type</label>
+                                <input type="text" class="form-control" id="EligType" name="EligType" value="HONOR GRADUATE ELIGIBILITY">
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label for="EffDate">Effective Date</label>
+                                <input type="date" class="form-control" id="EffDate" name="EffDate">
+                            </div>
+                        </div>
 
-                    <div class="row callout callout-info">
-                        <div class="col-3">
-                            <input type="text" class="form-control" placeholder="Last Name">
-                        </div>
-                        <div class="col-4">
-                            <input type="text" class="form-control" placeholder="First Name">
-                        </div>
-                        <div class="col-5">
-                            <input type="text" class="form-control" placeholder="Middle Name">
-                        </div>
                     </div>
-                </div>
+                    <div class="card card-info">
+                        <div class="card-header bg-teal">
+                            <h3 class="card-title">B. Personal Information Section</h3>
+                        </div>
+
+                    </div>
+                    <div class="card-body">
+                        <div class="form-row"> 
+                            <div class="form-group col-md-6">
+                                <label for="LName">Last Name</label>
+                                <input type="text" class="form-control" id="LName" name="LName" required placeholder="Last Name">
+                            </div>
+                            <div class="form-group col-md-5">
+                                <label for="FName">First Name</label>
+                                <input type="text" class="form-control" id="FName" name="FName"  required placeholder="First Name">
+                            </div>
+                            <div class="form-group col-md-1">
+                                <label for="ExtName">Extension Name</label>
+                                <input type="text" class="form-control" id="ExtName" name="ExtName" placeholder="Ext. Name">
+                            </div>
+                        </div>
+                        <div class="form-row">  
+                            <div class="form-group col-md-6">
+                                <label for="MdnName">Middle Name</label>
+                                <input type="text" class="form-control" id="MdnName" name="MdnName" placeholder="Middle Name">
+                            </div>
+                            <div class="form-group col-md-1">
+                            <label for="MI">Middle Initial</label>
+                                <input type="text" class="form-control" id="MI" name="MI" placeholder="Middle Initial">
+                            </div>
+                            <div class="form-group col-md-5">
+                                <label for="MName">Maiden Name</label>
+                                <input type="text" class="form-control" id="MName" name="MName" placeholder="Maiden Name">
+                            </div>
+
+
+                        </div>
+
+
+       
+
+
+                        </div>
+                       
+            </div>
+                </form>
+
 
 
 
