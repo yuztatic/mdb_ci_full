1d0548c826b4c72a5ee6c4d6009c3b1bd92d566d
unknown <juliusmanipol@gmail.com>
2024-04-02 08:00:26 +0800

    changed logo

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
-        <strong>Copyright &copy; 2024 SPELS ROV.</strong>
+        <strong>Copyright &copy; 2024 SPELS RO5.</strong>
         All rights reserved.
         <div class="float-right d-none d-sm-inline-block">
             <b>Version</b> 1.0.0
--- app/Views/layout/sidebar.php
+++ app/Views/layout/sidebar.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="index3.html" class="brand-link">
-        <img src="dist/img/csc_logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="dist/img/csc-logo-02.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">SPELS RO5</span>
     </a>
     <div class="sidebar">
