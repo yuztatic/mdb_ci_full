82a1e7a2cc0a3a7821b1f89e19e714503290720f
unknown <juliusmanipol@gmail.com>
2024-04-16 13:28:46 +0800

    back to reg code for testing higher educ

--- app/Controllers/DbTransTblHgeController.php
+++ app/Controllers/DbTransTblHgeController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use App\Models\DbreferenceRefRegionModel;
+use App\Models\DbrefereneRefHigherEducInstModel;
 use CodeIgniter\HTTP\ResponseInterface;
 
 class DbTransTblHgeController extends BaseController
@@ -13,6 +14,12 @@ class DbTransTblHgeController extends BaseController
         $refRegion = new DbreferenceRefRegionModel();
         $data['refRegion'] = $refRegion->where('RegCode', '05')->first();
 
+        // $refHigherEducInst = new DbrefereneRefHigherEducInstModel();
+        // $data['refHigherEducInst'] =$refHigherEducInst->where('geoRegCode', '05')->findAll();
+
         return view('hge/add', $data);
+
+
+
     }
 }
